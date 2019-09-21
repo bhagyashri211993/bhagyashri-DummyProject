d420da1ad606bb8cf6e263d096acda04bb2c13d2
bhagyashri211993 <bhagyashri211993@gmail.com>
2019-09-21 14:46:41 -0600

    name variable declared in b comp

--- src/app/shared/component/bhagyshri/bhagyshri.component.ts
+++ src/app/shared/component/bhagyshri/bhagyshri.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bhagyshri.component.css']
 })
 export class BhagyshriComponent implements OnInit {
+     public name="bhagyashri-pratiksha";
 
   constructor() { }
 
